22cfadb762b85cae290b4507085b054b3ebb924d
nikaasardanadze <nikushasardanadze56@gmail.com>
2024-03-13 15:54:36 +0400

    commit

---
+++ day 03/homework2/lesson3homework2.py
@@ -0,0 +1,37 @@
+6rit87y iyvguoiipyuh
+uh87yt6
+t6t6yy
+77yhuihiy
+88jnonoi
+jiuju8u8uu9i
+hyy8hiuj
+k\kkk[k\k\k\lambda]
+jnbuyguiho
+uiuhuj
+gbd
+PendingDeprecationWarningte
+knj
+m,l dfvgkldjfvm\
+rwjnogwog
+fiwu9jifr
+print&UUUUU
+inputU(H)
+IndentationError()
+oot+while
+out= StopAsyncIteration
+23fewbhi
+shu22i92
+njnn'2
+sssss5""
+2mkk
+22
+2dd
+2d
+
+2
+2ss
+2ss
+2s
+2s
+2
+s2
