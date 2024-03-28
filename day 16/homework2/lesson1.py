3c8878423e9763651bf83319ada65ab6adecfc0b
nikaasardanadze <nikushasardanadze56@gmail.com>
2024-03-28 19:28:48 +0400

    commit

---
+++ day 16/homework2/lesson1.py
@@ -0,0 +1,5 @@
+#შექმენით 2 სია და გააერთიანეთ ისინი
+
+list1=["goa","is","the best"]
+list2=["academy","in","the world"]
+print(list1 + list2)
---
+++ day 16/homework4/lesson1.py
@@ -0,0 +1,3 @@
+#შექმენით სია და სტრინგი ხოლო შემდეგ დაბეჭდეთ len ფუნქციის გამოყენებით მათი ზომა
+list=["asdafw","asbdius","asid","iorekawefg"]
+print(len(list))
