96b2667a94256e6d243ae3898cdb7de7cd69d203
nikaasardanadze <nikushasardanadze56@gmail.com>
2024-04-06 21:35:10 +0400

    commit

---
+++ day 18/lesson1.py
@@ -0,0 +1,60 @@
+#მომხმარებელს შემოატანინეთ ხუთი რიცხვი და ყველა შეიტანეთ სიაში. თქვენი დავალებაა, რომ დაბეჭდოთ ამ სიის ჯამი, არ გამოიყენოთ sum მეთოდი.
+
+# num1 = float(input("enter a number: "))
+# num2 = float(input("enter a number: "))
+# num3 = float(input("enter a number: "))
+# num4 = float(input("enter a number: "))
+# num5 = float(input("enter a number: "))
+
+# numbers = [num1, num2, num3, num4, num5]
+# print(numbers[0]+numbers[1]+numbers[2]+numbers[3]+numbers[4])
+
+
+#სიაში შეიტანეთ თქვენთვის სასურველი 10 რიცხვი. დაბეჭდეთ ამ სიაში არსებული ყველაზე დიდი რიცხვი, მინიშნება: გამოიყენეთ for ციკლი. არ გამოიყენოთ max მეთოდი.
+
+
+# numbers = [10, 6, 40, 23, 17, 29, 70, 51, 15, 30]
+# for num in numbers:
+#     if num >= 70:
+#         print(num)
+
+#სიაში შეიტანეთ 30-იდან 50-ის ჩათვლით არსებული ყველა რიცხვი. შემდეგ დაითვალეთ ამ სიაში არსებული კენტი რიცხვები და დაბეჭდეთ მათი რაოდენობა.
+
+# numbers = []
+# even_numbers = []
+# for i in range(30, 51):
+#  if i % 2 == 1:
+#   even_numbers.append(i)
+
+# print(len(even_numbers))    
+
+#სიაში შეიტანეთ 10-იდან 50-ის ჩათვლით არსებული 4-ის ჯერადი რიცხვები. შემდეგ შეაბრუნეთ ეს სია და დაბეჭდეთ ის, test case: [1, 2, 3, 4, 5] -> [5, 4, 3, 2, 1]
+
+numbers = [12, 16, 20, 24, 28, 32, 36, 40, 44, 48]
+numbers.reverse()
+print(numbers)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
